aab57c33adf7d1f82dbf1f862102d4b6810b8863
tyleroverholts <tyoverholts@gmail.com>
2022-12-09 14:43:41 -0700

    Added API URL to CreateAccount

--- ui/src/components/CreateAccount.js
+++ ui/src/components/CreateAccount.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 // import Context from './Context.js';
 import { useNavigate } from 'react-router-dom';
 import '../styles/CreateAccount.css'
+import config from './config.js';
+const ApiUrl = config[process.env.REACT_APP_NODE_ENV || "development"].apiUrl;
 
 const CreateAccount = () => {
   const navigate = useNavigate();
@@ -52,7 +54,7 @@ const CreateAccount = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     if(validateInputs()){
-      fetch('http://localhost:8080/CreateAccount', {
+      fetch(ApiUrl +'/CreateAccount', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
