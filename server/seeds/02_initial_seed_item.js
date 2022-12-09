818d388b0ea6043a529c98c681241326c0a49d7a
tyleroverholts <tyoverholts@gmail.com>
2022-12-09 14:27:22 -0700

    added seed for dockerfile

--- server/seeds/02_initial_seed_item.js
+++ server/seeds/02_initial_seed_item.js
@@ -7,5 +7,9 @@ exports.seed = async function(knex) {
   await knex('items').del()
   await knex('items').insert([
     {id: 1, user_id: 2, item_name: 'testitem', description: 'some words here', quantity: 1},
+    {id: 2, user_id: 2, item_name: 'testitem', description: 'some words here', quantity: 1},
+    {id: 3, user_id: 1, item_name: 'testitem', description: 'some words here', quantity: 1},
+    {id: 4, user_id: 1, item_name: 'testitem', description: 'some words here', quantity: 1},
+    {id: 5, user_id: 1, item_name: 'testitem', description: 'some words here', quantity: 1},
   ]);
 };
